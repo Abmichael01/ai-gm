6f97b929d972ee5d79fb0a07a65b6ceb807ca8d6
UrkelCodes <121265048+Abmichael01@users.noreply.github.com>
2025-04-05 09:52:47 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Footer from "./components/Footer";
 
 const App: React.FC = () => {
   return (
-    <div className="relative text-white overflow-hidden min-h-screen bg-slate-950 from-slate-900 to-slate-950">
+    <div className="relative flex flex-col text-white overflow-hidden min-h-screen bg-slate-950 from-slate-900 to-slate-950">
       <Navbar />
       <div className="px-3 sm:px-10 md:px-15 lg:px-20 pb-10 mt-10">
         <Home />
